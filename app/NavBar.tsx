23628336f699bc643f338d3f10c36967c2e34d74
Rebecca Romeo <rebecca.romeo@ryerson.ca>
2024-07-22 16:33:41 -0400

    build the nav bar

---
+++ app/NavBar.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Link from "next/link";
+import { FaBug } from "react-icons/fa";
+
+const links = [
+  {
+    label: "Dashboard",
+    href: "/",
+  },
+  {
+    label: "Issues",
+    href: "/issues",
+  },
+];
+
+const NavBar = () => {
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <FaBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <li>
+            <Link
+              className="text-zinc-500 hover:text-zinc-700 transition-colors"
+              key={link.label}
+              href={link.href}
+            >
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
